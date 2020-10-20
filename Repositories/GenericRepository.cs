968b444f8067b10b7a5bfb3c28d71107a6138947
Emílio Andrade <emilio.moreira.andrade@gmail.com>
2020-10-20 09:15:13 -0300

    First commit

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Backend.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Repositories
+{
+    public class GenericRepository<T> : IDisposable where T : class
+    {
+        private GenericContext context;
+
+        public GenericRepository()
+        {
+            context = new GenericContext();
+        }
+
+        public virtual void Add(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public virtual void Update(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public virtual T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
